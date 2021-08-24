d43c47c749d44fd3c95ae4d864cdfc7f5fc1fecf
Brian Reyman <brian@brianreyman.com>
2021-08-24 06:18:18 -0600

    Add Content Show and Hide eFunctions

--- primary-javascript-library.js
+++ primary-javascript-library.js
@@ -1,10 +1,35 @@
 // library of common Javascript functionality used by Brian Reyman.
 
-
 // replace current image with new image
-function changeImage(myImage, myElement){
-	currentImage = myImage;	
+function changeImage(myImage, myElement) {
+	currentImage = myImage;
 	var img = document.getElementById(myElement);
 	img.src = myImage;
 	return false;
+}
+
+// show/display all passed elements
+function showContent(...contentToDisplay) {
+	for (let myContent of contentToDisplay) {
+		// grab everything to show
+		let ShowContents = document.querySelectorAll(myContent);
+
+		// loop through the buttons using for..of and hide them
+		for (let showContent of ShowContents) {
+			showContent.style.display = 'block';
+		}
+	}
+}
+
+// hide all passed elements
+function hideContent(...contentToHide) {
+	for (let myContent of contentToHide) {
+		// grab everything to show
+		let HideContents = document.querySelectorAll(myContent);
+
+		// loop through the buttons using for..of and hide them
+		for (let hideContent of HideContents) {
+			hideContent.style.display = 'none';
+		}
+	}
 }
